d16338d9f706c9c9266f301978466dee1e4de779
webhawksitltd <sahid@webhawksit.com.bd>
2018-03-02 15:04:27 +0600

    Add p5 animation data

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var font; 
+var physics = []; 
+
+function preload(){
+	font = loadFont("AvenirNextLTPro-Demi.otf"); 	
+}
+
+function setup(){
+	//setup code 
+	
+	createCanvas(windowWidth,windowHeight);
+	background(51)
+	var points = font.textToPoints('MD . SAHID', windowWidth/4-150, windowHeight/2, 192); 
+	for(var i=0; i<points.length; i++ ){
+		var pt = points[i]
+		var physic = new Physics(pt.x, pt.y);
+		physics.push( physic );
+		// stroke(255);
+		// strokeWeight(8);
+		// point(pt.x, pt.y);
+	}
+}
+
+function draw(){
+	//Draw code here
+	background(51); 
+	for( var i = 0; i<physics.length; i++ ){
+		var v = physics[i];
+		v.behaviors();
+		v.update();
+		v.show();
+	}
+}
